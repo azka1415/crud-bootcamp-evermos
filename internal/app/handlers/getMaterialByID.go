25f3cf6228ad47f1724d5820199d1650f81bc95e
azka1415 <rafifzaydan4@gmail.com>
2023-07-23 00:45:11 +0700

    complete

--- internal/app/handlers/getMaterialByID.go
+++ internal/app/handlers/getMaterialByID.go
@@ -1,12 +1,12 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/azka1415/crud-bootcamp-evermos/internal/app/exceptions"
 	"github.com/azka1415/crud-bootcamp-evermos/internal/app/models"
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/responses"
 	"github.com/azka1415/crud-bootcamp-evermos/tools/utils"
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
@@ -15,18 +15,21 @@ import (
 func HandleGetMaterialByID(w http.ResponseWriter, r *http.Request) {
 	materialID, err := utils.ConvertToInt(chi.URLParam(r, "id"))
 	if err != nil {
-		exceptions.BadRequestException(w, err)
+		exceptions.BadParamException(w)
 		log.Error("invalid ID")
 		return
 	}
 
 	handleLogger := log.WithFields(log.Fields{"get": fmt.Sprintf("/materials/%v", materialID)})
+	materialService := models.MaterialService{}
+	m, err := materialService.GetByID(materialID)
 
-	var updatedMaterial models.Material
-	err = json.NewDecoder(r.Body).Decode(&updatedMaterial)
 	if err != nil {
-		exceptions.BadRequestException(w, err)
+		exceptions.NotFoundException(w, err)
 		handleLogger.Error(err)
 		return
 	}
+
+	responses.GetMaterialByIDResponse(w, m)
+	handleLogger.Info("Get Material By ID success")
 }
