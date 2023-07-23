8e1560368568120a655546f38edd2c7fc5dc27e3
azka1415 <rafifzaydan4@gmail.com>
2023-07-23 16:34:29 +0700

    more simplified

--- internal/app/responses/getAllMaterial.go
+++ internal/app/responses/getAllMaterial.go
@@ -7,16 +7,20 @@ import (
 	"github.com/azka1415/crud-bootcamp-evermos/internal/app/models"
 )
 
-type PaginatedResponse struct {
-	Response
+type Pagination struct {
 	Limit int `json:"limit"`
 	Page  int `json:"page"`
+}
+
+type PaginatedResponse struct {
+	Response
+	Pagination
 }
 
 func GetAllMaterialResponse(w http.ResponseWriter, m []models.Material, limit, page int) {
 	w.WriteHeader(http.StatusOK)
 	res := PaginatedResponse{
-		Response{Message: "Success", Data: m}, limit, page,
+		Response{Message: "Success", Data: m}, Pagination{Limit: limit, Page: page},
 	}
 	json.NewEncoder(w).Encode(res)
 }
