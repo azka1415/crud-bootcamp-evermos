6a0a18ea85e0f9e9233331e4a322a296548680f0
azka1415 <rafifzaydan4@gmail.com>
2023-07-23 21:22:00 +0700

    post material handler

--- internal/app/handlers/postMaterial.go
+++ internal/app/handlers/postMaterial.go
@@ -1,7 +1,35 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/exceptions"
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/models"
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/responses"
+	log "github.com/sirupsen/logrus"
+)
 
 func HandlePostMaterial(w http.ResponseWriter, r *http.Request) {
+	handleLogger := log.WithFields(log.Fields{"post": "/materials"})
 
+	var updatedMaterial models.UpdateMaterial
+	err := json.NewDecoder(r.Body).Decode(&updatedMaterial)
+
+	if err != nil {
+		exceptions.BadBodyException(w)
+		handleLogger.Error(err)
+		return
+	}
+	matService := models.NewMaterialService()
+
+	mat, err := matService.NewMaterial(updatedMaterial)
+
+	if err != nil {
+		exceptions.BadBodyException(w)
+		handleLogger.Error(err)
+		return
+	}
+
+	responses.NewMaterialResponse(w, mat)
 }
