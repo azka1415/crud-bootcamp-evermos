d11b280a7f552da5e0ef8d52f0cfd30dc81cb0c9
azka1415 <rafifzaydan4@gmail.com>
2023-07-23 21:21:26 +0700

    completed update by id

--- internal/app/handlers/updateMaterial.go
+++ internal/app/handlers/updateMaterial.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/azka1415/crud-bootcamp-evermos/internal/app/exceptions"
 	"github.com/azka1415/crud-bootcamp-evermos/internal/app/models"
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/responses"
 	"github.com/azka1415/crud-bootcamp-evermos/tools/utils"
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
@@ -21,11 +22,39 @@ func HandleUpdateMaterial(w http.ResponseWriter, r *http.Request) {
 	}
 
 	handleLogger := log.WithFields(log.Fields{"put": fmt.Sprintf("/materials/%v", materialID)})
+
+	matService := models.NewMaterialService()
+
+	exists, err := matService.MaterialExistsByID(materialID)
+
+	if err != nil {
+		exceptions.NotFoundException(w, err)
+		handleLogger.Error(err)
+		return
+	}
+
+	if !exists {
+		exceptions.NotFoundException(w, err)
+		handleLogger.Error(err)
+		return
+	}
+
 	var updatedMaterial models.UpdateMaterial
 	err = json.NewDecoder(r.Body).Decode(&updatedMaterial)
+	if err != nil {
+		exceptions.BadBodyException(w)
+		handleLogger.Error(err)
+		return
+	}
+
+	mat, err := matService.UpdateMaterial(materialID, updatedMaterial)
+
 	if err != nil {
 		exceptions.BadRequestException(w, err)
 		handleLogger.Error(err)
 		return
 	}
+
+	responses.UpdateMaterialResponse(w, mat)
+
 }
