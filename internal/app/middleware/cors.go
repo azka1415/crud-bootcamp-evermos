da95dd1e1e9a7406cf9e482c5b83bd9182fea789
azka1415 <rafifzaydan4@gmail.com>
2023-07-24 17:05:24 +0700

    axios related problem

--- internal/app/middleware/cors.go
+++ internal/app/middleware/cors.go
@@ -7,6 +7,10 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
