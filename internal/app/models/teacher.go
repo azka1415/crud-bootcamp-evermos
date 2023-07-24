96cc3cb62b2c936f676ab1851ed93d35ed83dff9
azka1415 <rafifzaydan4@gmail.com>
2023-07-24 10:39:42 +0700

    teacher service and get all

--- internal/app/models/teacher.go
+++ internal/app/models/teacher.go
@@ -1,7 +1,55 @@
 package models
 
+import (
+	"time"
+
+	"github.com/azka1415/crud-bootcamp-evermos/db"
+	"github.com/azka1415/crud-bootcamp-evermos/internal/app/repository"
+)
+
 type Teacher struct {
-	Id       int    `json:"id"`
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Position  string    `json:"position"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type PayloadTeacher struct {
 	Name     string `json:"name"`
 	Position string `json:"position"`
+}
+
+type TeacherService struct{}
+
+func NewTeacherService() *TeacherService {
+	return &TeacherService{}
+}
+
+func (t *TeacherService) GetAll(limit, page int, sort, field string) ([]Teacher, error) {
+
+	db, err := db.GetDB()
+	offset := (page - 1) * limit
+	if err != nil {
+		return nil, err
+	}
+	var teachers []Teacher
+
+	teaRepo := repository.NewTeacherRepository(db)
+	rows, err := teaRepo.GetAll(field, sort, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var teacher Teacher
+		rows.Scan(&teacher.Id, &teacher.Name, &teacher.Position, &teacher.CreatedAt, &teacher.UpdatedAt)
+		teachers = append(teachers, teacher)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return teachers, nil
 }
